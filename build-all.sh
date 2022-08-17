fe3a5f92228dbed1aaafb9472277b21a06feb662
Vernon Mauery <vernon@mauery.org>
2022-08-17 11:06:30 -0700

    build-all.sh to report progress
    Signed-off-by: Vernon Mauery <vernon@mauery.org>

--- build-all.sh
+++ build-all.sh
@@ -1,25 +1,54 @@
 #!/bin/sh
 
-rm -rf .test-builds
-mkdir -p .test-builds
+setup() {
+	rm -rf .test-builds
+	mkdir -p .test-builds
 
-# inform git we should ignore this dir
-echo "*" > .test-builds/.gitignore
+	# inform git we should ignore this dir
+	echo "*" > .test-builds/.gitignore
+}
 
-while read NAME CC CXX; do
-	for BACKEND in native boost gmp mpfr; do
-		echo -n "Building $NAME / $BACKEND: "
-		DIR=.test-builds/build-$NAME-$BACKEND
-		mkdir -p $DIR
-		CC=$CC CXX=$CXX meson -Dnumeric=$BACKEND $DIR >>$DIR/log 2>&1
-		ninja -v -C $DIR >>$DIR/log 2>&1
-		if [ $? -ne 0 ]; then
-			echo "FAIL!"
-		else
-			echo "OK"
+progress_watcher() {
+	local log="$1"
+	local msg="$2"
+	local v=""
+	while :; do
+		v=$(cat $log | grep "ninja: build stopped\|\[[0-9]\+/[0-9]\+\]" | sed "s,\[\([0-9]\+\)/\([0-9]\+\)\].*,\1/\2," | tail -n 1)
+		case "$v" in
+			"ninja: build stopped"*) return ;;
+			"") v=0;; 
+			*) v=$((100*$v)) ;;
+		esac
+		printf "$msg %02d%% " $v
+		if [ $v -eq 100 ]; then
+			return;
 		fi
+		sleep .1
 	done
-done <<-EOF
-	clang clang clang++
-	gcc gcc g++
-EOF
+}
+
+run() {
+	while read NAME CC CXX; do
+		for BACKEND in native boost gmp mpfr; do
+			MSG="\rBuilding $NAME / $BACKEND:  "
+			DIR=.test-builds/build-$NAME-$BACKEND
+			mkdir -p $DIR
+			CC=$CC CXX=$CXX meson -Dnumeric=$BACKEND $DIR >>$DIR/log 2>&1
+			progress_watcher $DIR/log "$MSG" &
+			ninja -v -C $DIR >>$DIR/log 2>&1
+			br=$?
+			wait %1
+			if [ $br -ne 0 ]; then
+				echo "FAIL!"
+			else
+				echo "OK"
+			fi
+		done
+	done <<-EOF
+		clang clang clang++
+		gcc gcc g++
+	EOF
+}
+
+setup
+run
