f3b766052e52e9ecec1b0d9b6711d089d5580216
Vernon Mauery <vernon@mauery.org>
2023-02-16 19:57:30 -0800

    Add log2 function
    Signed-off-by: Vernon Mauery <vernon@mauery.org>

--- functions/log.cpp
+++ functions/log.cpp
@@ -103,7 +103,56 @@ struct natural_logarithm : public CalcFunction
     }
 };
 
+struct log_base_two : public CalcFunction
+{
+    virtual const std::string& name() const final
+    {
+        static const std::string _name{"log2"};
+        return _name;
+    }
+    virtual const std::string& help() const final
+    {
+        static const std::string _help{
+            // clang-format off
+            "\n"
+            "    Usage: x log2\n"
+            "\n"
+            "    Returns the base-2 logarithm of the "
+            "bottom item on the stack: log2(x)\n"
+            // clang-format on
+        };
+        return _help;
+    }
+    virtual bool op(Calculator& calc) const final
+    {
+        return one_arg_conv_op(
+            calc,
+            [](const auto& a,
+               const units::unit&) -> std::tuple<numeric, units::unit> {
+                mpf log2 = log(mpf{2});
+                if constexpr (std::is_same<decltype(a), const mpc&>::value)
+                {
+                    return {log(a) / log2, units::unit()};
+                }
+                else
+                {
+                    if (a > decltype(a)(0))
+                    {
+                        return {log(mpf{a}) / log2, units::unit()};
+                    }
+                    else
+                    {
+                        return {log(mpc{a}) / log2, units::unit()};
+                    }
+                }
+            },
+            std::tuple<mpz, mpq>{}, std::tuple<mpf, mpf>{},
+            std::tuple<mpf, mpc>{});
+    }
+};
+
 } // namespace function
 
 register_calc_fn(logarithm);
 register_calc_fn(natural_logarithm);
+register_calc_fn(log_base_two);
