ebc9795ee0cbe46bd4251d2b4e3537fd8c6b798d
Vernon Mauery <vernon@mauery.org>
2023-12-15 08:52:40 -0800

    fix git-show-version.sh to notice tags
    Signed-off-by: Vernon Mauery <vernon@mauery.org>

--- git-show-version.sh
+++ git-show-version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-DESC=$(git describe 2>/dev/null)
-if [ $? -ne 0 ]; then
-	DESC=$(git rev-parse --short HEAD 2>/dev/null)
+DESC=$(git describe --tags 2>/dev/null)
+REV=$(git rev-parse --short HEAD 2>/dev/null)
+if [ -n "$DESC" ]; then
+	DESC="${DESC}-${REV}"
+else
+	DESC="$REV"
 fi
 if git status -uno | grep -q 'nothing to commit'; then
 	# nothing to do
