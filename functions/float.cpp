f5ba49992424d02e1ba059a8a810260c833cdbed
Vernon Mauery <vernon@mauery.org>
2023-12-18 14:11:14 -0800

    move ceil/floor/round to float.cpp
    An effort to reduce the size of arithmetic_funcs.cpp
    
    Signed-off-by: Vernon Mauery <vernon@mauery.org>

---
+++ functions/float.cpp
@@ -0,0 +1,160 @@
+/*
+Copyright © 2020 Vernon Mauery; All rights reserved.
+
+SPDX-License-Identifier: BSD-3-Clause
+*/
+#include <cmath>
+#include <function.hpp>
+#include <functions/common.hpp>
+
+namespace smrty
+{
+namespace function
+{
+
+struct ceil : public CalcFunction
+{
+    virtual const std::string& name() const final
+    {
+        static const std::string _name{"ceil"};
+        return _name;
+    }
+    virtual const std::string& help() const final
+    {
+        static const std::string _help{
+            // clang-format off
+            "\n"
+            "    Usage: x ceil\n"
+            "\n"
+            "    Returns the smallest integer greater than the bottom "
+            "item on the stack (round up)\n"
+            // clang-format on
+        };
+        return _help;
+    }
+    virtual bool op(Calculator& calc) const final
+    {
+        return one_arg_conv<ITypes<mpq>, OTypes<mpf>, LTypes<mpz, mpf, mpc>>::
+            op(calc,
+               [](const auto& a,
+                  const units::unit& ua) -> std::tuple<numeric, units::unit> {
+                   if constexpr (std::is_same<decltype(a), const mpc&>::value)
+                   {
+                       // complex adapter doesn't work with ceil
+                       mpf rp{ceil_fn(a.real())};
+                       mpf ip{ceil_fn(a.imag())};
+                       return {mpc(rp, ip), ua};
+                   }
+                   else if constexpr (std::is_same<decltype(a),
+                                                   const mpz&>::value)
+                   {
+                       // integers are already there
+                       return {a, ua};
+                   }
+                   else
+                   {
+                       return {ceil_fn(a), ua};
+                   }
+               });
+    }
+};
+
+struct floor : public CalcFunction
+{
+    virtual const std::string& name() const final
+    {
+        static const std::string _name{"floor"};
+        return _name;
+    }
+    virtual const std::string& help() const final
+    {
+        static const std::string _help{
+            // clang-format off
+            "\n"
+            "    Usage: x floor\n"
+            "\n"
+            "    Returns the smallest integer less than the bottom "
+            "item on the stack (round down)\n"
+            // clang-format on
+        };
+        return _help;
+    }
+    virtual bool op(Calculator& calc) const final
+    {
+        return one_arg_conv<ITypes<mpq>, OTypes<mpf>, LTypes<mpz, mpf, mpc>>::
+            op(calc,
+               [](const auto& a,
+                  const units::unit& ua) -> std::tuple<numeric, units::unit> {
+                   if constexpr (std::is_same<decltype(a), const mpc&>::value)
+                   {
+                       // complex adapter doesn't work with floor
+                       mpf rp{floor_fn(a.real())};
+                       mpf ip{floor_fn(a.imag())};
+                       return {mpc(rp, ip), ua};
+                   }
+                   else if constexpr (std::is_same<decltype(a),
+                                                   const mpz&>::value)
+                   {
+                       // integers are already there
+                       return {a, ua};
+                   }
+                   else
+                   {
+                       return {floor_fn(a), ua};
+                   }
+               });
+    }
+};
+
+struct round : public CalcFunction
+{
+    virtual const std::string& name() const final
+    {
+        static const std::string _name{"round"};
+        return _name;
+    }
+    virtual const std::string& help() const final
+    {
+        static const std::string _help{
+            // clang-format off
+            "\n"
+            "    Usage: x round\n"
+            "\n"
+            "    Returns the nearest integer to the bottom "
+            "item on the stack (classic round)\n"
+            // clang-format on
+        };
+        return _help;
+    }
+    virtual bool op(Calculator& calc) const final
+    {
+        return one_arg_conv<ITypes<mpq>, OTypes<mpf>, LTypes<mpz, mpf, mpc>>::
+            op(calc,
+               [](const auto& a,
+                  const units::unit& ua) -> std::tuple<numeric, units::unit> {
+                   if constexpr (std::is_same<decltype(a), const mpc&>::value)
+                   {
+                       // complex adapter doesn't work with round
+                       mpf rp{round_fn(a.real())};
+                       mpf ip{round_fn(a.imag())};
+                       return {mpc(rp, ip), ua};
+                   }
+                   else if constexpr (std::is_same<decltype(a),
+                                                   const mpz&>::value)
+                   {
+                       // integers are already there
+                       return {a, ua};
+                   }
+                   else
+                   {
+                       return {round_fn(a), ua};
+                   }
+               });
+    }
+};
+} // namespace function
+} // namespace smrty
+
+register_calc_fn(ceil);
+register_calc_fn(floor);
+register_calc_fn(round);
