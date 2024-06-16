4429dbe4d4932a18cb3a78c5ed8babaf95ad0d76
Vernon Mauery <vernon@mauery.org>
2024-06-16 14:27:39 -0700

    add -v option to main to change default verbosity
    Signed-off-by: Vernon Mauery <vernon@mauery.org>

---
+++ main.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <exception>
+#include <iostream>
+#include <span>
+#include <string_view>
+#include <vector>
+
+// int cpp_main(const std::span<std::string_view>& args);
+int cpp_main(const std::vector<std::string_view>& args);
+
+int main(int argc, char** argv)
+{
+    // create a span of string views
+    std::vector<std::string_view> args{argv, argv + argc};
+    return cpp_main(args);
+}
+
+#define main(...) cpp_main(__VA_ARGS__)
--- main_cli.cpp
+++ main_cli.cpp
@@ -5,13 +5,39 @@ SPDX-License-Identifier: BSD-3-Clause
 */
 
 #include "debug.hpp"
+#include "main.hpp"
 
 #include <calculator.hpp>
 #include <string>
 #include <vector>
 
-int main()
+int usage(const std::vector<std::string_view>& args)
 {
+    std::print(stderr, "Usage: {} [-v [n]]\n", args[0]);
+    return 1;
+}
+
+int cpp_main(const std::vector<std::string_view>& args)
+{
+    for (size_t i = 1; i < args.size(); i++)
+    {
+        std::string_view arg = args[i];
+        if (arg == "-v")
+        {
+            if ((i + 1) < args.size())
+            {
+                arg = args[++i];
+                int v{};
+                const auto& [ptr, ec] =
+                    std::from_chars(arg.begin(), arg.end(), v);
+                if (ec != std::error_code{} || ptr != arg.end())
+                {
+                    return usage(args);
+                }
+                lg::debug_level = static_cast<lg::level>(v);
+            }
+        }
+    }
     smrty::Calculator& calc = smrty::Calculator::get();
     try
     {
@@ -19,8 +45,8 @@ int main()
     }
     catch (const std::exception& e)
     {
-        lg::error("uncaught exception: {}\n\nPress Enter to continue...\n",
-                  e.what());
+        lg::emergency("uncaught exception: {}\n\nPress Enter to continue...\n",
+                      e.what());
         std::string nextline;
         std::getline(std::cin, nextline);
     }
